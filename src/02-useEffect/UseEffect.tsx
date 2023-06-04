dd07735f75f955a1637ba6148f60d261a1dd3065
Artem Magei <artemmagej@gmail.com>
2023-06-04 12:19:15 +0300

    learn: useEffect

--- src/02-useEffect/UseEffect.tsx
+++ src/02-useEffect/UseEffect.tsx
@@ -1,5 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 export const UseEffect: FC = () => {
-  return <div>UseEffect</div>;
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json));
+  }, [resourceType]);
+
+  const handlePosts = () => setResourceType('posts');
+  const handleUsers = () => setResourceType('users');
+  const handleComments = () => setResourceType('comments');
+
+  return (
+    <>
+      <div>
+        <button onClick={handlePosts}>Posts</button>
+        <button onClick={handleUsers}>Users</button>
+        <button onClick={handleComments}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item: object, index) => {
+        return <pre key={index}>{JSON.stringify(item)}</pre>;
+      })}
+    </>
+  );
 };
