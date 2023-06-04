9e6dd9bd6b60979dbc13b101a5cd12f4360bc439
Artem Magei <artemmagej@gmail.com>
2023-06-04 14:29:45 +0300

    learn: useTransition

--- src/08-useTransition/UseTransition.tsx
+++ src/08-useTransition/UseTransition.tsx
@@ -1,5 +1,28 @@
-import { FC } from 'react';
+import React, { FC, useState, useTransition } from 'react';
 
 export const UseTransition: FC = () => {
-  return <div>UseTransition</div>;
+  const [isPending, startTransition] = useTransition();
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState<string[]>([]);
+  const LIST_SIZE = 9000;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    startTransition(() => {
+      const tempList = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        tempList.push(e.target.value + i);
+      }
+      setItems(tempList);
+    });
+  };
+
+  return (
+    <>
+      <input onChange={handleInputChange} type={'text'} value={input} />
+      {isPending
+        ? 'Loading...'
+        : items.map(item => <div key={item}>{item}</div>)}
+    </>
+  );
 };
