bffe9135f57c3d875e53d3555225edc3cca6df9b
Artem Magei <artemmagej@gmail.com>
2023-06-04 12:19:05 +0300

    learn: useState

--- src/01-useState/UseState.tsx
+++ src/01-useState/UseState.tsx
@@ -1,5 +1,22 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 export const UseState: FC = () => {
-  return <div>UseState</div>;
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('blue');
+
+  const handleDecrementCount = () => setCount(prevCount => prevCount - 1);
+
+  const handleIncrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+    setTheme('red');
+  };
+
+  return (
+    <div>
+      <button onClick={handleDecrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={handleIncrementCount}>+</button>
+    </div>
+  );
 };
